42340ab0eb4c60663191e6dd5a622e85d5a82570
ADEYEMO MUSODIQ OLALEKAN <104383885+Musodiq-adeyemo@users.noreply.github.com>
2023-03-21 05:04:00 +0000

    Update test_grading.py

--- api/tests/test_grading.py
+++ api/tests/test_grading.py
@@ -40,41 +40,6 @@ class StudentGradeTestCase(unittest.TestCase):
 
         assert response.json == []
 
-    def test_create_studentGrade(self):
-        data = {
-            'course_id' : 1,
-            'student_id' : 1,
-            'email' : 'sirmuso@gmail.com',
-            'level ': 101,
-            'matric_no' : 'ALT/2023/STUDENT/1',
-            'score' : 79,
-            'course_unit' : 3,
-            'grade' : 'A',
-            'grade_point' : 12
-
-        }
-        token = create_access_token(identity='sirmuso@gmail.com')
-
-        headers = {
-            'Authorization':f"Bearer {token}"
-        }
-
-        response = self.client.post('Grade/grades',json=data,headers =headers)
-
-        assert response.status_code == 200
-
-        grades = Grade.query.all()
-
-        grade_id = grades[0].id
-
-        assert grade_id == 1
-
-        assert len(grades) == 1
-
-        assert response.json['score']== 79
-
-        assert response.json['grade']== 'A'
-
 
     def test_get_studentgrade_by_id(self):
         grade = Grade(
